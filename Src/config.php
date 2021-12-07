b6aa999210b350780d8b777b3c2f401ab1f6e669
Ahmet-Salman <ahmad.salman@ug.bilkent.edu.tr>
2021-12-07 11:28:02 +0300

    Create config.php

---
+++ Src/config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'dijkstra.ug.bcc.bilkent.edu.tr');
+   define('DB_USERNAME', 'ahmad.salman');
+   define('DB_PASSWORD', 'ikHBfw98');
+   define('DB_DATABASE', 'ahmad_salman');
+   $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+?>
