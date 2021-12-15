269c9532bb1563cd078c56d49aaeb694e448c4d1
Ahmet-Salman <ahmad.salman@ug.bilkent.edu.tr>
2021-12-15 14:43:48 +0300

    Added navbar

--- src/Customer/bookingDetails.php
+++ src/Customer/bookingDetails.php
@@ -1,9 +1,9 @@
 <?php
 include("../session.php");
+require_once(getRootDirectory()."/util/navbar.php");
 $cid = $_SESSION['id'];
 
 $bookId = $_GET['bookId'];
-echo "Booking with ID $bookId";
 
 $sql = "SELECT hotel.name, hotel.city, hotel.address, hotel.rating, hotel.phone, room.price, room.type, booking.start_date, booking.end_date, booking.type AS bType
 FROM booking, hotel, room
@@ -18,42 +18,29 @@ $row = $result->fetch_assoc();
 <html lang="en">
 
 <head>
-    <title>Customer Dashboard</title>
+    <title>Booking Details</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="../styles/navbar.php">
-    <link rel="stylesheet" href="../styles/bookingDetails.php">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 
 <body>
-<div style="border: 2px solid red; border-radius: 5px;" class="pill-nav">
-        <a href="../customer">Home</a>
-        <a href="reserveFlight.php">Reserve a Flight</a>
-        <a href="pastTours.php">Past Tours</a>
-        <a href="bookTour.php">Book a Tour</a>
-        <a href="reserveHotel.php">Reserve a Hotel</a>
-        <a href="profile.php">Profile</a>
-        <form action="../logout.php">
-            <input type="submit" name="logout" class="btn btn-danger" value="Logout" />
-        </form>
-    </div>
+    <?php
+        echo getCustomerNav("./");
+    ?>
     <h3>Booking Details of Your Reservation <?php ?></h3>
     <div style="border: 2px solid red; border-radius: 5px;" class="container">
-    <br>
-  <p><b>Hotel Name:</b> <?php echo $row['name']?> </p>
-  <p><b>Hotel Address:</b> <?php echo $row['address']. " / " . $row['city']?></p>
-  <p><b>Hotel Phone Number: </b> <?php echo $row['phone']?></p>
-  <p><b>Hotel Rating:</b> <?php echo $row['rating']. " stars"?></p>
-  <p><b>Booking Start Date:</b> <?php echo $row['start_date']?> </p>
-  <p><b>Booking End Date:</b> <?php echo $row['end_date']?></p>
-  <p><b>Room Type:</b> <?php echo $row['type']?></p>
-  <p><b>Room Price:</b> <?php echo $row['price']. "$"?></p>
-  <p><b>Booking Done via:</b> <?php echo $row['bType']?></p>
-  <br>
-</div>
+        <br>
+        <p><b>Hotel Name:</b> <?php echo $row['name']?> </p>
+        <p><b>Hotel Address:</b> <?php echo $row['address']. " / " . $row['city']?></p>
+        <p><b>Hotel Phone Number: </b> <?php echo $row['phone']?></p>
+        <p><b>Hotel Rating:</b> <?php echo $row['rating']. " stars"?></p>
+        <p><b>Booking Start Date:</b> <?php echo $row['start_date']?> </p>
+        <p><b>Booking End Date:</b> <?php echo $row['end_date']?></p>
+        <p><b>Room Type:</b> <?php echo $row['type']?></p>
+        <p><b>Room Price:</b> <?php echo $row['price']. "$"?></p>
+        <p><b>Booking Done via:</b> <?php echo $row['bType']?></p>
+        <br>
+    </div>
 </body>
 
 
--- src/util/navbar.php
+++ src/util/navbar.php
@@ -41,4 +41,26 @@
         <!-- End of Navbar -->';
         return $navbar;
     }
+    function getCustomerNav () : string {
+        $navbar = '<head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="../styles/navbar.php">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    </head>
+    <div style="border: 2px solid red; border-radius: 5px;" class="pill-nav">
+        <a href="../customer">Home</a>
+        <a href="reserveFlight.php">Reserve a Flight</a>
+        <a href="pastTours.php">Past Tours</a>
+        <a href="bookTour.php">Book a Tour</a>
+        <a href="reserveHotel.php">Reserve a Hotel</a>
+        <a href="profile.php">Profile</a>
+        <form action="../logout.php">
+            <input type="submit" name="logout" class="btn btn-danger" value="Logout" />
+        </form>
+    </div>';
+        return $navbar;
+    }
 ?>
