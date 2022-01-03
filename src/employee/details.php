2a90714725a38185a8adb2901b343463ae9b4887
MUHAMMED CAN KÜÇÜKASLAN <muhammedcankucukaslan@gmail.com>
2022-01-04 01:29:15 +0300

    fix: employee/details.php navbar

--- src/employee/details.php
+++ src/employee/details.php
@@ -27,13 +27,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST')
   <title>Tour Details</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="../styles/navbar.php">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
     <?php
-        echo getEmployeeNavBar("./");
+        require_once("navbar.php");
 
         $ts_id = $_SESSION['ts_id'];
         $ts = TourSection::makeTourSection($conn, $ts_id);
